d78716d726e6021ad846ddbdee3cd82189895ba8
royjuntak <rms951124@gmail.com>
2018-09-07 07:01:30 -0700

    fix conflic

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-iiii::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
