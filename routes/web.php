39c9f5c1e8dc4b8b54776980239a47e11c695ee1
royjuntak <rms951124@gmail.com>
2018-09-07 06:37:42 -0700

    add admin page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('admin', function () {
+    return view('admin');
 });
